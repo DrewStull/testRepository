d5cb019324375a79005c90c3f57e898f6dc56a99
Drew <drew.stull@yahoo.com>
2023-01-19 12:55:52 -0600

    Task 1. Add who is saying 'hello'.

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,5 +1,6 @@
 class HelloWorldApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!); //displaying the string
+		System.out.println("Say's George."); //task -1
 	}
 }
